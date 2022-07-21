e462b1fd9b8f470853c95e9a44318d26bad95a7b
samschool789@gmail.com <samschool789@gmail.com>
2022-07-20 21:58:03 -0500

    end of lesson

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
+
 // const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 // const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
@@ -65,5 +66,20 @@ const config = {
   ],
   mode: 'development'
 };
+new WebpackPwaManifest({
+  name: "Food Event",
+  short_name: "Foodies",
+  description: "An app that allows you to view upcoming food events.",
+  start_url: "../index.html",
+  background_color: "#01579b",
+  theme_color: "#ffffff",
+  fingerprints: false,
+  inject: false,
+  icons: [{
+    src: path.resolve("assets/img/icons/icon-512x512.png"),
+    sizes: [96, 128, 192, 256, 384, 512],
+    destination: path.join("assets", "icons")
+  }]
+})
 
 module.exports = config;
